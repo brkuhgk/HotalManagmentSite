55fcc5b6290eb56a901a071797da548163666dde
karthik <43676649+brkuhgk@users.noreply.github.com>
2023-06-13 01:17:49 -0400

    Add files via upload

---
+++ admin/include/db_config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_SERVER','localhost');
+    define('DB_USERNAME','srinithDineshKarthik');
+    define('DB_PASSWORD','srinithDineshKarthikPass');
+    define('DB_DATABASE','srinithDineshKarthikDatabase');
+
+?>
